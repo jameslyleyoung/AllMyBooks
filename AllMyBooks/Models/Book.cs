e935fe547fd11d0865244e1e5e960daedea9055b
James Young <jameslyleyoung@gmail.com>
2017-11-22 01:14:56 -0600

    init commit

---
+++ AllMyBooks/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllMyBooks.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public int ISBN { get; set; }
+        public string Title { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
